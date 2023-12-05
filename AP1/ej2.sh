ff0e7b92b3163f2b2f690f1594107d3d4a0a5c35
Dani Arastell <daarro@floridauniversitaria.es>
2023-12-05 10:37:44 +0100

    modificando ej1 y creando ej2

---
+++ AP1/ej2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Introduce un número mayor que 0"
+read numero
+
+if [ $numero -gt 0 ] 
+then
+if  (($numero %2 == 0))
+then
+  echo "El número es par."
+else
+  echo "Introduce un número válido."
+fi
+else
+echo "El numero no es mayor que 0"
+fi
